8e1e50fb7b9872722da5e9eac82df46e1d0cbbc6
fran <flietal240@g.educaand.es>
2024-06-03 23:33:54 +0200

    Refactorizacion

--- src/main/java/com/fran/cooperativa/backend/infrastructure/adapter/IInvoiceCrudRepository.java
+++
@@ -1,7 +0,0 @@
-package com.fran.cooperativa.backend.infrastructure.adapter;
-
-import com.fran.cooperativa.backend.infrastructure.entity.InvoiceEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface IInvoiceCrudRepository extends CrudRepository<InvoiceEntity, Long> {
-}
